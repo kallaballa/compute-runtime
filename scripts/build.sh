b0359905764ac2047d59cb6c54fb408ba99446c9
Jacek Danecki <jacek.danecki@intel.com>
2018-04-27 02:19:38 +0200

    ci: build Neo with IGC binaries on Travis, Shippable, Semaphore
    IGC binaries are used from specific Neo release.
    IGC sources are synchronized with binary release.

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright (c) 2018, Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+mkdir workspace; cd workspace
+git clone https://github.com/intel/gmmlib gmmlib
+git clone --depth 1 -b release-1.7.0 https://github.com/google/googlemock gmock
+git clone --depth 1 -b release-1.7.0 https://github.com/google/googletest gtest
+git clone --depth 1 https://github.com/KhronosGroup/OpenCL-Headers khronos
+git clone ../../compute-runtime neo
+pushd neo/scripts/igc ; ./prepare.sh ; popd
+mkdir build; cd build
+cmake -DBUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release ../neo
+make -j 2 package
+
