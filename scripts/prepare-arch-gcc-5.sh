d81b28882d1d68db544ec4a6a2cc35f7d3b8f419
Jacek Danecki <jacek.danecki@intel.com>
2018-05-18 14:37:51 +0200

    fix for compilation with gcc-5 on arch

--- scripts/prepare-arch-gcc-5.sh
+++ scripts/prepare-arch-gcc-5.sh
@@ -25,5 +25,7 @@ ln -s /usr/bin/gcc-5 /usr/bin/gcc
 ln -s /usr/bin/g++-5 /usr/bin/g++
 useradd -m build -g wheel
 sed -i "s/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
+sed -i "s/ -fno-plt//g" /etc/makepkg.conf
 cp -a /root/*.sh /home/build
 su -l build /home/build/build-arch-dep.sh
+
