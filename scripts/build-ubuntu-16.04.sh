6a85f468fdf94c21901eac5632aa941af2ca08f0
Jacek Danecki <jacek.danecki@intel.com>
2018-05-31 02:04:02 +0200

    set IGC and FCL library names in build scripts
    - these names are set by IGC CMakeLists.txt
    
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-ubuntu-16.04.sh
+++ scripts/build-ubuntu-16.04.sh
@@ -26,6 +26,8 @@ git clone --depth 1 ../../compute-runtime neo
 pushd neo/scripts/igc ; ./prepare.sh ; popd
 mkdir build; cd build
 
-cmake -G Ninja -DBUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release ../neo
+cmake -G Ninja -DBUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release \
+    -DIGC_LIBRARY_NAME="igdccl" -DFCL_LIBRARY_NAME="igdfcl" \
+    ../neo
 ninja
 
