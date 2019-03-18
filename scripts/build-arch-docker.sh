1f5b7d1b7e8390a2b6ed9edb411be3759985e716
Jacek Danecki <jacek.danecki@intel.com>
2019-03-18 18:46:19 +0100

    build Neo components on Arch
    - limit number of make jobs to 1 for IGC build
    
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-arch-docker.sh
+++ scripts/build-arch-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 Intel Corporation
+# Copyright (C) 2018-2019 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
 
 git fetch -t
 git clone ../compute-runtime neo
-docker build -f scripts/docker/Dockerfile-arch-ppa-gcc-8 -t neo-arch-ppa-gcc-8:ci .
+docker build -f scripts/docker/Dockerfile-arch-gcc-8 -t neo-arch-gcc-8:ci .
 
