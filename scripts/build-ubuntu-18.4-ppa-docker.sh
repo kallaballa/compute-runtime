d33e7107f3c4d67ed00d64cce06914889c30a006
Jacek Danecki <jacek.danecki@intel.com>
2019-03-26 15:33:06 +0100

    use newer packages from ppa:jdanecki/intel-opencl
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-ubuntu-18.4-ppa-docker.sh
+++ scripts/build-ubuntu-18.4-ppa-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 Intel Corporation
+# Copyright (C) 2018-2019 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
 
 git fetch -t
 git clone ../compute-runtime neo
-docker build -f scripts/docker/Dockerfile-ubuntu-18.04-ppa-gcc-5 -t neo-ubuntu-18.04-ppa-gcc-5:ci .
+docker build -f scripts/docker/Dockerfile-ubuntu-18.04-ppa -t neo-ubuntu-18.04-ppa:ci .
 
