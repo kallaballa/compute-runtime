6eb2d58ececb216c6a9f1ffa9c701895a80646f1
Jacek Danecki <jacek.danecki@intel.com>
2019-03-29 15:04:21 +0100

    Add spirv-llvm-translator build
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-arch-dep.sh
+++ scripts/build-arch-dep.sh
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: MIT
 #
 
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/spirv-llvm-translator.tar.gz
+tar -xzf spirv-llvm-translator.tar.gz
+cd spirv-llvm-translator
+makepkg -i --noconfirm
+cd ..
+
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/intel-opencl-clang.tar.gz
 tar -xzf intel-opencl-clang.tar.gz
 cd intel-opencl-clang
