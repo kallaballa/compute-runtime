adec2b7705cc4c85507cb378e57a58e9b9a4b892
Jacek Danecki <jacek.danecki@intel.com>
2021-04-19 15:18:26 +0200

    login to docker hub
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-fedora32-copr-docker.sh
+++ scripts/build-fedora32-copr-docker.sh
@@ -7,5 +7,10 @@
 
 git fetch -t
 git clone ../compute-runtime neo
+if [ -z "$DOCKER_USERNAME" ]
+then
+   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+fi
 docker info
 docker build -f scripts/docker/Dockerfile-fedora-32-copr -t neo-fedora-32-copr:ci .
+
