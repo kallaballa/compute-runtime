bbec3478446af411455c9886ea152a2a72cf97a9
Jacek Danecki <jacek.danecki@intel.com>
2021-04-20 15:58:48 +0200

    build different Gen
    - gen 8 and 9 on Fedora 32
    - gen 11 and 12 on Fedora 33
    
    Signed-off-by: Jacek Danecki <jacek.danecki@intel.com>

--- scripts/build-fedora33-copr-docker.sh
+++ scripts/build-fedora33-copr-docker.sh
@@ -8,5 +8,5 @@
 git fetch -t
 git clone ../compute-runtime neo
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -f scripts/docker/Dockerfile-fedora-32-copr -t neo-fedora-32-copr:ci .
+docker build -f scripts/docker/Dockerfile-fedora-33-copr -t neo-fedora-33-copr:ci .
 
